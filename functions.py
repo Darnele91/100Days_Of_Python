b943d8d858f93f2aadbce4441885aa47cedcbbf7
Darnele Adhemar <darnele.adhemar@ibm.com>
2018-12-10 16:15:28 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+def addTwoNumbers(a, b):
+    c = a + b
+    return c
+
+
+def multiplyTwoNumbers(a, b):
+    c = a * b
+    return c
+
+
+def divideTwoNumbers(a, b):
+    c = a / b
+    return c
+
+
+print(addTwoNumbers(3, 4))
+print(multiplyTwoNumbers(3, 4))
+print(divideTwoNumbers(3, 4))
